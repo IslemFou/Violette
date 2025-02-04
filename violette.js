b992cb517a6f4252d85170706a948d85e477c00e
ISLEM FOURATI <fourati.islem@outlook.fr>
2025-02-04 19:27:58 +0100

    JavaScript file added

---
+++ violette.js
@@ -0,0 +1,13 @@
+// alert("js est activé !")
+let form = document.querySelector("#form");
+let clientButton = document.querySelector("#client");
+let profButton = document.querySelector("#professionnel");
+
+
+form.addEventListener("submit", function(event)
+{
+    event.preventDefault();
+
+
+
+});
